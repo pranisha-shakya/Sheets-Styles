41d2b062a1685e1a661c720c6845dbcc86ba69d6
SERENDIPITY-19 <pranishaashakya@gmail.com>
2025-02-25 21:05:50 +0545

    Add project files.

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sheets_Styles.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/ShopController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sheets_Styles.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
